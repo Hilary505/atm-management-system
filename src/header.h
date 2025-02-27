194d178c6fd8c645e85bea137d632963d9b71bf7
hilaromondi <omondihilary19@gmail.com>
2025-02-27 21:52:04 +0300

    feat: Add transferAccount function to header

--- src/header.h
+++ src/header.h
@@ -45,5 +45,6 @@ void updateAccount(struct User u);
 void checkAccount(struct User u);
 void makeTransaction(struct User u);
 void removeAccount(struct User u);
+void transferAccount(struct User u);
 
 #endif
