779342abfc9bbb159351f8d0229c3298ed43a8ae
hilaromondi <omondihilary19@gmail.com>
2025-02-27 05:56:57 +0300

    feat: Add register function

--- src/auth.c
+++ src/auth.c
@@ -2,6 +2,7 @@
 #include "header.h"
 
 char *USERS = "./data/users.txt";
+int id[10];
 
 void loginMenu(char a[50], char pass[50])
 {
@@ -56,4 +57,89 @@ const char *getPassword(struct User u)
 
     fclose(fp);
     return "no user found";
+}
+
+
+
+void registerMenu(struct User *u) { 
+    FILE *fp;
+    struct User userChecker;
+    int userExists = 0;
+    char name[50], password[50]; // Declare variables
+    int newId = 0;
+
+    system("clear");
+    printf("\n\n\n\t\t\t\t Bank Management System\n\n\n");
+
+    printf("\n\n\t\t\t\tEnter name: ");
+    scanf("%s", name);
+
+    // Check if username already exists
+    if ((fp = fopen("./data/users.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
+    }
+
+    while (fscanf(fp, "%d %s %s",&userChecker.id, userChecker.name, userChecker.password) != EOF) {
+        if (strcmp(userChecker.name, name) == 0) {
+            userExists = 1;
+            break;
+        }
+         if (userChecker.id >= newId) {
+                newId = userChecker.id + 1; // Find the next available ID
+        }
+    }
+    fclose(fp);
+
+    if (userExists) {
+        printf("\n\t\t\t\tUsername already taken!\n");
+        printf("\n\t\t\t\tPress any key to continue...");
+        getchar(); // Consume newline
+        getchar();
+        return;
+    }
+
+    // Handle password input with hidden characters
+    struct termios oflags, nflags;
+
+    // Disable echo
+    tcgetattr(fileno(stdin), &oflags);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
+
+
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
+        perror("tcsetattr");
+        exit(1);
+    }
+
+    printf("\n\n\t\t\t\tEnter Password: ");
+    scanf("%s", password);
+
+    // Restore terminal
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0) {
+        perror("tcsetattr");
+        exit(1);
+    }
+
+    // Save new user to file
+    fp = fopen("./data/users.txt", "a");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    fprintf(fp, "%d %s %s \n",newId, name, password);
+    fclose(fp);
+
+    printf("\n\n\t\t\t\tRegistration Successful!\n");
+    printf("\n\n\t\t\t\tPress any key to continue");
+    getchar();
+    getchar();
+
+    u->id = newId;
+    strcpy(u->name,name);
+    strcpy(u->password,password);
+
 }
